7f4aa16ae42dccf87936799dabe8b05bc8c0f363
quassbottle <quassbottleyt@gmail.com>
2026-02-22 00:18:06 +0300

    feat: update MatchCreateParams type to include mode property

--- apps/backend/src/modules/match/types/index.ts
+++ apps/backend/src/modules/match/types/index.ts
@@ -1,7 +1,6 @@
 import { DbMatchCreateParams } from 'lib/infrastructure/db';
-import { ModesEnum } from 'osu-web.js';
 
 export type MatchCreateParams = Omit<
   DbMatchCreateParams,
   'createdAt' | 'updatedAt' | 'id' | 'omit'
-> & { mode: ModesEnum };
+>;
