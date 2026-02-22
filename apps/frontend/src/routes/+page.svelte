7624925e0911171eb9cbaa376b45e0148022b06c
quassbottle <quassbottleyt@gmail.com>
2026-02-22 17:40:21 +0300

    refactor: simplify +page.svelte by removing unused components and updating banner content

--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -1,29 +1,10 @@
 <script lang="ts">
 	import Banner from '$lib/components/banner/banner.svelte';
-	import Group from '$lib/components/group/group.svelte';
-	import TabGroup from '$lib/components/tabGroup/tabGroup.svelte';
-</script>
+	</script>
 
 <div class="flex flex-col gap-16">
 	<Banner class="bg-[#2c2c2c] text-white" let:Title let:Description>
-		<Title>Banner Title</Title>
-		<Description>Banner Description</Description>
+		<Title>Welcome to CTE tournaments!</Title>
+		<Description>Sample text xd</Description>
 	</Banner>
-
-	<Group let:Title let:Content>
-		<Title>Test</Title>
-		<Content>Hello, world!</Content>
-	</Group>
-
-	<TabGroup let:Head let:ContentItem>
-		<Head let:Item>
-			<Item>Tab 1</Item>
-			<Item>Tab 2</Item>
-			<Item>Tab 3</Item>
-		</Head>
-
-		<ContentItem>Tab 1 content</ContentItem>
-		<ContentItem>Tab 2 content</ContentItem>
-		<ContentItem>Tab 3 content</ContentItem>
-	</TabGroup>
 </div>
